1fb6e463bf4325b804bbf1342bb8155d8608e1ff
Gabriel Hepper <110434463+GabrielHepper@users.noreply.github.com>
2022-11-24 20:07:02 -0300

    Create Segundo_Usuário_amaral.sql

---
+++ Segundo_Usuário_amaral.sql
@@ -0,0 +1,12 @@
+Gabriel Hepper
+
+USE DOTA2;
+
+SELECT * FROM primeiro_dia;
+SELECT * FROM item_mais_caro;
+SELECT * FROM patrocinio_mais_caro;
+SELECT * FROM id_do_ganhador;
+SELECT * FROM campeoes;
+
+INSERT INTO patrocinadores VALUES
+('36', 'Gás tem', 'R$', '30.000');
