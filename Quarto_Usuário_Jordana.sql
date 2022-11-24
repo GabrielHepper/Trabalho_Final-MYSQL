d7eeb67c4abcd37f971eb1c34eb6baf3e4be4c01
Gabriel Hepper <110434463+GabrielHepper@users.noreply.github.com>
2022-11-24 20:09:12 -0300

    Create Quarto_Usuário_Jordana.sql

---
+++ Quarto_Usuário_Jordana.sql
@@ -0,0 +1,14 @@
+Gabriel Hepper
+
+USE DOTA2;
+
+SELECT * FROM partida;
+SELECT * FROM herois;
+SELECT * FROM itens;
+SELECT * FROM jogadores;
+SELECT * FROM patrocinadores;
+SELECT * FROM times;
+SELECT * FROM vencedor;
+
+INSERT INTO patrocinadores VALUES
+('36', 'Gás tem', 'R$', '30.000');
