9f7bb6ae3d9b9ea52f0b78705b10c28fd847cbd8
Gabriel Hepper <110434463+GabrielHepper@users.noreply.github.com>
2022-11-24 20:08:05 -0300

    Create Terceiro_Usuário_gilberto.sql

---
+++ Terceiro_Usuário_gilberto.sql
@@ -0,0 +1,32 @@
+Gabriel Hepper
+
+USE DOTA2;
+
+INSERT INTO partida VALUES
+('16', '35', '36', '25/12/18');
+
+INSERT INTO herois VALUES
+('75', 'temabubu', '75');
+
+INSERT INTO itens VALUES
+('44', 'pasta de pasta', '999');
+
+INSERT INTO jogadores VALUES
+('75', 'Renatinha', '75', '35');
+
+INSERT INTO patrocinadores VALUES
+('36', 'Gás tem', 'R$', '30.000');
+
+INSERT INTO times VALUES
+('36', 'falaboca', '36');
+
+INSERT INTO vencedor VALUES
+('36', 'falaboca', 'Um carro zero', '5');
+
+SELECT * FROM partida;
+SELECT * FROM herois;
+SELECT * FROM itens;
+SELECT * FROM jogadores;
+SELECT * FROM patrocinadores;
+SELECT * FROM times;
+SELECT * FROM vencedor;
